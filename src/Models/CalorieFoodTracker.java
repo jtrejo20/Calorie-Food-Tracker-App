71532ea6bf02400c6acf1def0e69575267deccd3
jtrejo-19 <128644977+jtrejo-19@users.noreply.github.com>
2023-12-21 13:13:29 -0700

    Added CalorieFoodTracker.java

---
+++ src/Models/CalorieFoodTracker.java
@@ -0,0 +1,17 @@
+package Models;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class CalorieFoodTracker {
+	//Hash Map with each day of week as a key, and each value is a 
+	//  size 2 Object array containing food, calorie count
+	private HashMap<String, Object[]> weeklyFoodTrack;
+	public CalorieFoodTracker() {
+		
+	}
+	
+	
+	
+	
+}
