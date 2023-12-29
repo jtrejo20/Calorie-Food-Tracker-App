e7929fdd9237073ed9471b392ab82a765ef101d1
jtrejo-19 <128644977+jtrejo-19@users.noreply.github.com>
2023-12-29 08:01:32 -0700

    Added weekly calorie chart and enter day button

--- src/Views/TrackerGUI.java
+++ src/Views/TrackerGUI.java
@@ -6,6 +6,10 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -24,7 +28,13 @@ public class TrackerGUI extends Application {
 	private BorderPane statsPane;
 	private VBox foodCaloriePane;
 	private ScrollPane scroller;
-	private TextField dataInput;
+	private TextField dayOfWeekField;
+	private Button dayOfWeekButton;
+	private GridPane dayOfWeekGrid;
+	private Label dayOfWeekLabel;
+	private BarChart<String,Number> weeklyCalorieChart;
+	private final CategoryAxis xAxis = new CategoryAxis();
+    private final NumberAxis yAxis = new NumberAxis();
 	
 	public static void main(String[] args) {
 		
@@ -36,7 +46,7 @@ public class TrackerGUI extends Application {
 		this.stage = stage;
 	    stage.setTitle("Calorie & Food Tracker App");
 	    layoutGUI();
-	    Scene trackerSceneAlpha = new Scene(mainPane, 740, 600);
+	    Scene trackerSceneAlpha = new Scene(mainPane, 840, 600);
 	    stage.setScene(trackerSceneAlpha);
 	    stage.show();
 	}
@@ -64,13 +74,42 @@ public class TrackerGUI extends Application {
 		statsPane.setTop(scroller);
 		
 		
+		dayOfWeekLabel = new Label("Enter Day #");
+		dayOfWeekField = new TextField("1");
+		dayOfWeekButton = new Button("Enter");
+		dayOfWeekGrid = new GridPane();
 		
+		dayOfWeekGrid.add(dayOfWeekLabel, 0, 0);
+		dayOfWeekGrid.add(dayOfWeekField, 1, 0);
+		dayOfWeekGrid.add(dayOfWeekButton, 1, 1);
 		
+		dayOfWeekField.setMaxWidth(50);
+		
+		statsPane.setBottom(dayOfWeekGrid);
+		
+		createBarChart();
 		
 		mainPane = new GridPane();
 		mainPane.add(statsPane,0,0);
+		mainPane.add(weeklyCalorieChart, 1, 0);
+	}
+	
+	private void createBarChart() {
 		
-//	    mainPane.setCenter(theButton);
+        weeklyCalorieChart = new BarChart<String,Number>(xAxis,yAxis);
+        weeklyCalorieChart.setTitle("Weekly Calorie Consumption Summary");
+        xAxis.setLabel("Days of the Week");       
+        yAxis.setLabel("Calories");
+        XYChart.Series series1 = new XYChart.Series();       
+        series1.getData().add(new XYChart.Data("Monday", 0.0));
+        series1.getData().add(new XYChart.Data("Tuesday", 0.0));
+        series1.getData().add(new XYChart.Data("Wednesday", 10.0));
+        series1.getData().add(new XYChart.Data("Thursday", 30.0));
+        series1.getData().add(new XYChart.Data("Friday", 12.0));   
+        series1.getData().add(new XYChart.Data("Saturday", 23.0));
+        series1.getData().add(new XYChart.Data("Sunday", 20.0));
+        
+        weeklyCalorieChart.getData().add(series1);
 	}
 
 	/**
